60716919ab0102f117e0f47c4a452bebb38195d2
Paulo Machado <paulormachado95@gmail.com>
2026-05-09 20:59:06 -0300

    Initialize Spring Boot project with Maven wrapper, application properties, and user model

---
+++ src/main/java/com/example/raizes_do_nordeste/enums/Perfil.java
@@ -0,0 +1,8 @@
+package com.example.raizes_do_nordeste.enums;
+
+public enum Perfil {
+    ADMIN,
+    GERENTE,
+    ATENDENTE,
+    CLIENTE
+}
---
+++ src/main/java/com/example/raizes_do_nordeste/model/Usuario.java
@@ -0,0 +1,100 @@
+package com.example.raizes_do_nordeste.model;
+
+import com.example.raizes_do_nordeste.enums.Perfil;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+    @Column(nullable = false, unique = true)
+    private String cpf;
+    @Column(nullable = false)
+    private Boolean consentimentoLgpd;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Perfil perfil;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email, String senha, String cpf, Boolean consentimentoLgpd, Perfil perfil) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.consentimentoLgpd = consentimentoLgpd;
+        this.perfil = perfil;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getConsentimentoLgpd() {
+        return consentimentoLgpd;
+    }
+
+    public void setConsentimentoLgpd(Boolean consentimentoLgpd) {
+        this.consentimentoLgpd = consentimentoLgpd;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+ 
+
+}
